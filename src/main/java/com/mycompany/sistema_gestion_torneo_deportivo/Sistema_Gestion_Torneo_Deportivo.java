8061a712d9f71815adf7a93fb531942aca10aa4b
nataliarolonleal29 <nataliarolonleal@gmail.com>
2026-09-11 17:10:27 -0500

    docs: :memo: creé el repositorio y los archivos
    creé el repositorio y los archivos

---
+++ src/main/java/com/mycompany/sistema_gestion_torneo_deportivo/Sistema_Gestion_Torneo_Deportivo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_gestion_torneo_deportivo;
+
+/**
+ *
+ * @author Natalia Rolon Leal
+ */
+public class Sistema_Gestion_Torneo_Deportivo {
+
+    public static void main(String[] args) {
+        
+       
+        
+    }
+}
